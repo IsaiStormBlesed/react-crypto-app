040347d94afeecd4ca22865761c5ca01954e1dde
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2022-01-27 19:26:54 +0000

    API implemented and error boundary too.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,8 @@
 import useSelectCoins from "../hooks/useSelectCoins";
 import { currencies } from "../data/helperData";
 import styled from "@emotion/styled";
+import { useEffect, useState } from "react";
+import Error from "./Error";
 
 const InputSubmit = styled.input`
   background-color: #9497ff;
@@ -23,18 +25,59 @@ const InputSubmit = styled.input`
 
 const Form = () => {
 
+  const [error, setError] = useState(false);
+  const [cryptos, setCryptos] = useState([]);
   const [currency, SelectCoins] = useSelectCoins('Choose your currency', currencies)
-  console.log(currency);
-  return (
-    <form>
-      <SelectCoins 
+  const [crypto, SelectCrypto] = useSelectCoins('Choose your Crypto Coin', cryptos)
 
-      />
-      <InputSubmit 
-        type="submit" 
-        value="Quote" 
-      />
-    </form>
+  
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+      const response = await fetch(url)
+      const data = await response.json()
+
+      const cryptoArray = data.Data.map((crypto) => {
+        const cryptoObject = {
+          id: crypto.CoinInfo.Name,
+          name: crypto.CoinInfo.FullName
+        }
+
+        return cryptoObject
+      })
+      
+      setCryptos(cryptoArray)
+    }
+
+    fetchAPI()
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    if ([currency, crypto].includes('')) {
+      setError(true)
+      return
+    }
+
+    setError(false)
+  }
+  
+
+  return (
+    <>
+      { error && <Error >All fields are necessary</Error> }
+
+      <form onSubmit={handleSubmit}>
+        <SelectCoins />
+        <SelectCrypto />
+
+        <InputSubmit 
+          type="submit" 
+          value="Quote" 
+        />
+      </form>
+    </>
   )
 };
 
